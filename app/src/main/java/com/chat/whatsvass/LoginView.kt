18627923e22b923719244893f7918348970c3a02
VASS\daniel.hidalgop <daniel.hidalgop@vasscompany.com>
2024-03-07 17:26:11 +0100

    Login design preview

--- app/src/main/java/com/chat/whatsvass/LoginView.kt
+++ app/src/main/java/com/chat/whatsvass/LoginView.kt
@@ -6,23 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,130 +24,89 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class LoginView : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Login()
+            LoginScreen()
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Login(modifier: Modifier = Modifier) {
+fun LoginScreen() {
     Box(
-        modifier = modifier
-            .requiredWidth(width = 393.dp)
-            .requiredHeight(height = 852.dp)
-            .clip(shape = RoundedCornerShape(20.dp))
-            .background(color = Color(0xff8091f2))
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff8091f2))
     ) {
-        TextButton(
-            onClick = { },
-            shape = RoundedCornerShape(24.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+        Column(
             modifier = Modifier
-                .align(alignment = Alignment.Center)
-                .offset(x = (-0.5).dp,
-                    y = 246.dp)
-                .requiredWidth(width = 200.dp)
-                .requiredHeight(height = 64.dp)
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Box(
-                modifier = Modifier
-                    .requiredWidth(width = 200.dp)
-                    .requiredHeight(height = 64.dp)
-            ) {
-                Text(
-                    text = "Login",
-                    color = Color(0xfff2f2f2),
-                    textAlign = TextAlign.Center,
-                    style = TextStyle(
-                        fontSize = 12.sp,
-                        fontWeight = FontWeight.Bold),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .wrapContentHeight(align = Alignment.CenterVertically))
-            }
+            Image(
+                painter = painterResource(id = R.drawable.logo), // Asegúrate de tener tu logo en res/drawable
+                contentDescription = "Logo",
+                modifier = Modifier.size(218.dp),
+                contentScale = ContentScale.Fit
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            UserTextField()
+            Spacer(modifier = Modifier.height(16.dp))
+            PasswordTextField()
+            Spacer(modifier = Modifier.height(16.dp))
+            LoginButton(onClick = {
+                // Lógica para manejar el inicio de sesión
+            })
+            Spacer(modifier = Modifier.height(16.dp))
+            CreateAccountText()
         }
-        TextField(
-            value = "",
-            onValueChange = {},
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = Color(0xfff2f2f2)),
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 58.dp,
-                    y = 481.dp)
-                .requiredWidth(width = 276.dp)
-                .requiredHeight(height = 64.dp))
-        Text(
-            text = "Crear Usuario",
-            color = Color.White,
-            textAlign = TextAlign.Center,
-            style = TextStyle(
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold),
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 117.dp,
-                    y = 785.dp)
-                .requiredWidth(width = 151.dp)
-                .requiredHeight(height = 45.dp)
-                .wrapContentHeight(align = Alignment.CenterVertically))
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = "logo 3",
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 87.dp,
-                    y = 92.dp)
-                .requiredSize(size = 218.dp))
-        Text(
-            text = "contraseña\n",
-            color = Color(0xff274073),
-            textAlign = TextAlign.Center,
-            style = TextStyle(
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold),
-            modifier = Modifier
-                .align(alignment = Alignment.BottomStart)
-                .offset(x = 58.dp,
-                    y = (-307).dp)
-                .requiredWidth(width = 276.dp)
-                .requiredHeight(height = 64.dp)
-                .wrapContentHeight(align = Alignment.Bottom))
-        Text(
-            text = "Usuario",
-            color = Color(0xff274073),
-            textAlign = TextAlign.Center,
-            style = TextStyle(
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold),
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 58.dp,
-                    y = 382.dp)
-                .requiredWidth(width = 276.dp)
-                .requiredHeight(height = 64.dp)
-                .wrapContentHeight(align = Alignment.CenterVertically))
     }
 }
 
-@Preview(widthDp = 393, heightDp = 852)
 @Composable
-private fun LoginPreview() {
-    Login(Modifier)
+fun UserTextField() {
+    var user by remember { mutableStateOf("") }
+    TextField(
+        value = user,
+        onValueChange = { user = it },
+        label = { Text("Usuario") }
+    )
 }
+
+@Composable
+fun PasswordTextField() {
+    var password by remember { mutableStateOf("") }
+    TextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Contraseña") }
+    )
+}
+
+@Composable
+fun LoginButton(onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text("Login")
+    }
+}
+
+@Composable
+fun CreateAccountText() {
+    Text(
+        text = "Crear usuario",
+        color = Color.White,
+        modifier = Modifier.clickable {
+            // Lógica para manejar el click en el texto "Crear usuario"
+        }
+    )
+}
+

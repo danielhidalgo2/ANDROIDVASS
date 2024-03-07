11479d729c4532e1d347fc5bcf1bd3108bcdf197
VASS\daniel.hidalgop <daniel.hidalgop@vasscompany.com>
2024-03-07 11:11:36 +0100

    Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,12 @@ android {
 }
 
 dependencies {
+    //COMPOSE
+    implementation ("androidx.compose.ui:ui:1.6.2")
+    implementation ("androidx.compose.material:material:1.6.2")
+    implementation ("androidx.activity:activity-compose:1.8.2")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
